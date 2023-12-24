ee3f73384c3a1419fe53bf009bcce29dc13451f7
VadimLNN <svobodolubsev@inbox.ru>
2023-12-24 22:48:32 +0700

    написана операция +

--- Podpivko5/Podpivko5/Program.cs
+++ Podpivko5/Podpivko5/Program.cs
@@ -17,6 +17,9 @@ namespace Podpivko5
 
             Console.WriteLine("Select operation + - * /");
             string op = Console.ReadLine();
+
+            if (op == "+")
+                Console.WriteLine(A + B);
         }
     }
 }
